d5a4bce6fc5fe1365bd70a649cc8333a7a87c213
Usman Akram <usman.akram99@hotmail.com>
2023-09-07 20:45:09 +0500

    holding model added

--- models/holding.js
+++ models/holding.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const holdingSchema = new mongoose.Schema({
-  assetName: { type: String, required: true },
-  assetQuantity: { type: Number, required: true },
-  baseCurrency: { type: String, required: true },
+  baseAsset: { type: mongoose.Schema.Types.ObjectId, ref: 'Asset', required: true },
+  quoteAsset: { type: mongoose.Schema.Types.ObjectId, ref: 'Asset', required: true },
+  quantity: { type: Number, required: true },
   price: { type: Number, required: true },
   portfolio: { type: mongoose.Schema.Types.ObjectId, ref: 'Portfolio', required: true },
   purchasedAt: { type: Date, required: true, default: Date.now },
